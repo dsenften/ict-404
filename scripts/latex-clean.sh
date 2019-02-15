ad1b4cdddcdb934a55c2633e118fbab6816e0da3
Daniel Senften <daniel@senften.org>
2019-02-15 08:35:36 +0100

    Delete BlueJ generated files.

--- scripts/latex-clean.sh
+++ scripts/latex-clean.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Remove temporary TeX files
 rm -f *.aux
 rm -f *.loe
 rm -f *.lof
@@ -16,6 +17,11 @@ rm -f *.vrb
 
 rm -rf _minted*
 
+# Generated BlueJ files
+find .. -name "*.class" -exec rm "{}" \;
+find .. -name "*.ctxt" -exec rm "{}" \;
+
+# Generated .pdf files
 rm -f exam.pdf
 rm -f main.article.pdf
 rm -f main.beamer.pdf
