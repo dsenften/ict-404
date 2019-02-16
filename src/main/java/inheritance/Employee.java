f8d652ef356dc4121dc3108a396980cd4cfd8810
Daniel Senften <daniel@senften.org>
2019-02-16 18:57:31 +0100

    Remove static variable ID

--- src/main/java/inheritance/Employee.java
+++ src/main/java/inheritance/Employee.java
@@ -6,13 +6,11 @@ import lombok.ToString;
 @Getter
 @ToString
 public class Employee {
-    private static int ID = 0;
     private final String firstName;
     private final String lastName;
 
     public Employee(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        Employee.ID++;
     }
 }
