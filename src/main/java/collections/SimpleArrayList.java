22c51a5c8b2f74aacd4caa42224a484858004c81
Daniel Senften <daniel@senften.org>
2019-03-15 18:11:37 +0100

    Suppress compiler warnings

--- src/main/java/collections/SimpleArrayList.java
+++ src/main/java/collections/SimpleArrayList.java
@@ -1,21 +1,19 @@
 package collections;
 
-import inheritance.Employee;
+
+import academy.inheritance.Employee;
 
 import java.util.ArrayList;
 
+@SuppressWarnings({"MismatchedQueryAndUpdateOfCollection", "unchecked"})
 public class SimpleArrayList {
 
     public static void main(String[] args) {
 
         ArrayList list = new ArrayList();
 
-        Employee emp = new Employee("Daniel", "Senften");
-
         list.add(1);
         list.add("Daniel");
-        list.add(emp);
-
-
+        list.add(new Employee("Daniel", "Senften"));
     }
 }
